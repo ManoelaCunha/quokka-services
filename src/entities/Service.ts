867c0dba0b70544b4b706d0bd537400595a91085
Rafael Schug <rafaelschugbc@gmail.com>
2022-04-18 10:11:35 -0300

    feat: add services entity

---
+++ src/entities/Service.ts
@@ -0,0 +1,31 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Resident } from './Resident';
+import { Category } from './Category';
+import { ServiceProvider } from './ServiceProvider';
+
+@Entity('services')
+export class Service {
+    @PrimaryGeneratedColumn('uuid')
+    service_id: string;
+
+    @Column({ nullable: false })
+    title: string;
+
+    @Column({ type: 'text', nullable: false })
+    description: string;
+
+    @Column({ default: 'available', nullable: false })
+    status: string;
+
+    @ManyToOne(() => Resident, (resident: Resident) => resident.services)
+    resident: Resident;
+
+    @ManyToOne(() => Category, (category: Category) => category.services)
+    category: Category;
+
+    @ManyToOne(
+        () => ServiceProvider,
+        (serviceProvider: ServiceProvider) => serviceProvider.services,
+    )
+    serviceProvider: ServiceProvider;
+}
