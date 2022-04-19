f795ac5f83f800750b45fd61dcf2a786d7e20638
Victor Scherer <victor_scherer@hotmail.com>
2022-04-19 15:35:40 -0300

    feat: SuperAdmin login shape created

--- src/shapes/index.ts
+++ src/shapes/index.ts
@@ -1,5 +1,6 @@
 import createShape from './serviceProvider/create.shape';
 import loginShape from './serviceProvider/login.shape';
 import createSuperAdminShape from './SuperAdmin/create.shape';
+import loginSuperAdminShape from './SuperAdmin/login.shape';
 
-export { createShape, loginShape, createSuperAdminShape };
+export { createShape, loginShape, createSuperAdminShape, loginSuperAdminShape };
