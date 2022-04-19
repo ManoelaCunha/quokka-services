40e5e662917815f80ce41e67ff8d6127cc04da76
Victor Scherer <victor_scherer@hotmail.com>
2022-04-19 15:30:17 -0300

    feat: SuperAdmin shape created

---
+++ src/shapes/index.ts
@@ -0,0 +1,5 @@
+import createShape from './serviceProvider/create.shape';
+import loginShape from './serviceProvider/login.shape';
+import createSuperAdminShape from './SuperAdmin/create.shape';
+
+export { createShape, loginShape, createSuperAdminShape };
