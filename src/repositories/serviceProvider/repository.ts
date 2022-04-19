1f30ca0bb79887b29cb4cb1c799c2432d6b63bca
nafly09 <juniorinterlyte95@hotmail.com>
2022-04-19 11:10:03 -0300

    feat: finished service provider repository

---
+++ src/repositories/serviceProvider/repository.ts
@@ -0,0 +1,29 @@
+import { Repository, getRepository } from 'typeorm';
+import Provider from '../../entities/ServiceProvider';
+import ServiceProviderRepo from './interfaces';
+
+class ServiceProviderRespository implements ServiceProviderRepo {
+    private ormRepository: Repository<Provider>;
+
+    constructor() {
+        this.ormRepository = getRepository(Provider);
+    }
+
+    saveProvider = async (provider: Provider) =>
+        await this.ormRepository.save(provider);
+
+    findProvider = async () => await this.ormRepository.find();
+
+    findProviderById = async (uuid: string) => {
+        return await this.ormRepository.findOne({ where: { uuid } });
+    };
+
+    updateProvider = async (uuid: string, data: Partial<Provider>) => {
+        return await this.ormRepository.update(uuid, data);
+    };
+
+    deleteProvider = async (uuid: string) =>
+        await this.ormRepository.delete(uuid);
+}
+
+export default ServiceProviderRespository;
