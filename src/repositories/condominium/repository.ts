0e3500580755c8e3e73107fd7231605f8ba71a4f
Vinícius de Freitas <viniciusfreitas.1.4.4@gmail.com>
2022-04-19 15:56:11 -0300

    fix: creating method findByEmail in condominium repository

--- src/repositories/condominium/repository.ts
+++ src/repositories/condominium/repository.ts
@@ -26,6 +26,16 @@ class CondominiumRepository implements ICondominiumRepository {
         return await this.ormRepository.findOne(condominium_id);
     };
 
+    findCondominiumByEmail = async (
+        condominium_email: string,
+    ): Promise<ICondominium> => {
+        return await this.ormRepository.findOne({
+            where: {
+                email: condominium_email,
+            },
+        });
+    };
+
     updateCondominium = async (
         condominium: ICondominium,
     ): Promise<ICondominium> => {
