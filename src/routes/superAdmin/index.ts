eef97b10d91d86d743d35d3c9b5553f7da0428c6
Vinícius de Freitas <viniciusfreitas.1.4.4@gmail.com>
2022-04-20 20:22:50 -0300

    fix: changing the middleware in get by id route

--- src/routes/superAdmin/index.ts
+++ src/routes/superAdmin/index.ts
@@ -41,7 +41,7 @@ const superAdminRoutes = (app: Application) => {
 
     router.get(
         '/super_adm/:id',
-        authToken(SuperAdminRepository),
+        validateToken(SuperAdminRepository),
         retrieveSuperAdminById,
     );
 
