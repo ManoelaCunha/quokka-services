6418dcded49508fcd90de41443c55c36d520ae6a
Vinícius de Freitas <viniciusfreitas.1.4.4@gmail.com>
2022-04-20 22:18:24 -0300

    feat: implementing the getCondominiumGetByID route

--- src/routes/condominium/index.ts
+++ src/routes/condominium/index.ts
@@ -1,7 +1,10 @@
 import { Router, Application } from 'express';
-import { createCondomonium } from '../../controllers';
+import { createCondomonium, retrieveCondominiumById } from '../../controllers';
 import { validateShape, validateToken } from '../../middlewares';
-import { SuperAdminRepository } from '../../repositories';
+import {
+    CondominiumRepository,
+    SuperAdminRepository,
+} from '../../repositories';
 import { createCondominium as createCondominiumShape } from '../../shapes';
 
 const router = Router();
@@ -13,6 +16,13 @@ const condominiumRoutes = (app: Application) => {
         validateShape(createCondominiumShape),
         createCondomonium,
     );
+
+    router.get(
+        '/condominiums/:id',
+        validateToken(CondominiumRepository),
+        retrieveCondominiumById,
+    );
+
     app.use(router);
 };
 
