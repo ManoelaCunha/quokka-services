2c91d2d97fc5b842d36f7dc24862b4e5cd53c0db
Vinícius de Freitas <viniciusfreitas.1.4.4@gmail.com>
2022-04-20 21:33:24 -0300

    feat: implementing the getAll condominiums route

---
+++ src/controllers/condominiums/getAll.controller.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import Condominium from '../../entities/Condominium';
+import { CondominiumRepository } from '../../repositories';
+
+const getAllCondominiums = async (req: Request, res: Response) => {
+    const condominiums = await new CondominiumRepository().findCondominium();
+
+    const newCondominiums: Array<Partial<Condominium>> = [];
+
+    condominiums.map((condominium: Condominium) => {
+        const { trusteePassword, ...condominiumWithoutPassword } = condominium;
+
+        newCondominiums.push(condominiumWithoutPassword);
+    });
+
+    return res.status(200).json(newCondominiums);
+};
+
+export default getAllCondominiums;
