81cdfa4ba356c1e713d7e1c1d38ed6ff8004aa47
Vinícius de Freitas <viniciusfreitas.1.4.4@gmail.com>
2022-04-22 18:26:14 -0300

    fix: fixing the findCondominiumById method and the findAllCondominiums method in route

--- src/controllers/condominiums/getById.controller.ts
+++ src/controllers/condominiums/getById.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import { CondominiumRepository } from '../../repositories';
 
 const retrieveCondominiumById = async (req: Request, res: Response) => {
-    const condominiums = await new CondominiumRepository().findCondominiumById(
+    const condominiums = await new CondominiumRepository().findById(
         req.params.id,
     );
 
