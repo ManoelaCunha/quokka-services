3a78972f536923f9e3119a6a6f68f4d3a91e10eb
Vinícius de Freitas <viniciusfreitas.1.4.4@gmail.com>
2022-04-26 15:43:32 -0300

    fix: implementing the logic to return the relationships data

--- src/controllers/condominiums/getById.controller.ts
+++ src/controllers/condominiums/getById.controller.ts
@@ -1,23 +1,17 @@
 import { Request, Response } from 'express';
 import { CondominiumRepository } from '../../repositories';
+import { getCondominiumByIdService } from '../../services';
 
 const retrieveCondominiumById = async (req: Request, res: Response) => {
     const condominiums = await new CondominiumRepository().findById(
         req.params.id,
     );
 
-    const {
-        trusteePassword,
-        residents,
-        condominiumServiceProviders,
-        ...condominiumWithouPasword
-    } = condominiums;
+    const { trusteePassword, ...condominiumWithouPasword } = condominiums;
 
-    return res.status(200).json({
-        ...condominiumWithouPasword,
-        residents,
-        serviceProviders: condominiumServiceProviders,
-    });
+    getCondominiumByIdService(condominiumWithouPasword);
+
+    return res.status(200).json({ ...condominiumWithouPasword });
 };
 
 export default retrieveCondominiumById;
