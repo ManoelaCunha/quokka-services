9c7c63f0f5602c47f6fa39ca5508e3fb87afa2a6
Manoela Fernanda Girello Cunha <manoela_fg@hotmail.com>
2022-04-28 19:35:11 -0300

    fix: update express.d.ts

--- src/@types/express.d.ts
+++ src/@types/express.d.ts
@@ -3,8 +3,14 @@ import ServiceProvider from '../entities/ServiceProvider';
 import SuperAdmin from '../entities/SuperAdmin';
 import Resident from '../entities/Resident';
 import Condominium from '../entities/Condominium';
+import Service from '../entities/Service';
 
-type validatedTypes = ServiceProvider | SuperAdmin | Resident | Condominium;
+type validatedTypes =
+    | ServiceProvider
+    | SuperAdmin
+    | Resident
+    | Condominium
+    | Service;
 
 declare global {
     namespace Express {
